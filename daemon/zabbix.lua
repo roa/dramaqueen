cf7c9602036fc0749e665ec2c81e2fdd766dac72
Robert Abraham <r.abraham@mueller-wulff.com>
2012-08-31 12:04:18 +0200

    added explaination to example daemon script

--- daemon/zabbix.lua
+++ daemon/zabbix.lua
@@ -1,7 +1,20 @@
 #!/usr/bin/lua
 
-checkTime = 1
--- script    = "/home/roa/programming/dramaqueen/script/test.lua"
+-- checkTime: specifies the interval in seconds in which
+-- the script is called. only integer values are acceptable
+checkTime = 30
+
+-- script: specifies the script name which the daemon should call.
+-- do NOT give the full path, only the name. the working directory is specified
+-- in the main config file. you should ensure that this script is executable for 
+-- the dramaqueen user.
+-- dramaqueen notifies you only if this script produces output. so it is your turn to ensure,
+-- that it only prints to std::out it you want a message.
 script    = "zabbix.lua"
+
+-- recipients is a table of user, who should be notified by dramaqueen.
 recipients = { "roa@localhost" }
+
+-- hosts is a table of hosts, on which dramaqueen tries to execute the script.
+-- dramaqueen will not inform you, if a host could not be reached.
 hosts = { "localhost:9898" }
